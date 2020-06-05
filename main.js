2d2510e421c28d7f5d7710ef858b8252e7679841
xfated <taykaiyang556@gmail.com>
2020-06-05 09:46:24 +0800

    change load syntax

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $("#image-input").change(function(){
     readURL(this);
 });
 
-$("#captured-image").load(function(){
+$("#captured-image").on('load',function(){
     console.log('change detected');
     predictions();
 })
